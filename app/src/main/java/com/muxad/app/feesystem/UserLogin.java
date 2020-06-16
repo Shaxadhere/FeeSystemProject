bf402bafe841d12a11942158e58a623cba2a739d
Shahzad Ahmad <shahzad3766952@outlook.com>
2020-06-16 18:57:49 +0500

    splash init

--- app/src/main/java/com/muxad/app/feesystem/UserLogin.java
+++ app/src/main/java/com/muxad/app/feesystem/UserLogin.java
@@ -1,4 +1,17 @@
 package com.muxad.app.feesystem;
 
-public class UserLogin {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserLogin extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+        setTitle("Student Login");
+
+
+    }
 }
